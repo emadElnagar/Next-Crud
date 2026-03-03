d917aa36f167485f1be4f90059e62b397355cb75
unknown <emad.m.alnagar@gmail.com>
2026-03-03 22:15:14 +0200

    Create a new item - API

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import connectToDatabase from "@/app/lib/mongodb";
 import Item from "@/app/models/Item";
 
@@ -7,4 +7,32 @@ export async function GET() {
   await connectToDatabase();
   const items = await Item.find();
   return NextResponse.json(items);
+}
+
+// Create a new item
+export async function POST(req: NextRequest) {
+  await connectToDatabase();
+  try {
+    const { name, content } = await req.json();
+    if (!name || !content) {
+      return NextResponse.json(
+        { message: "Name and content are required" },
+        { status: 400 },
+      );
+    }
+    const nameExists = await Item.findOne({ name });
+    if (nameExists) {
+      return NextResponse.json(
+        { message: "Item name already exists" },
+        { status: 400 },
+      );
+    }
+    const newItem = new Item({ name, content });
+    await newItem.save();
+    return NextResponse.json(newItem, { status: 201 });
+  } catch (error: unknown) {
+    const message =
+      error instanceof Error ? error.message : "Internal server error";
+    return NextResponse.json(message);
+  }
 }
